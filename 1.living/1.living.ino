c579707004318dfbecb3a4a19a5453972aa62008
Marcelo Lara <marcelo.lara@globant.com>
2020-08-27 23:03:27 -0300

    light to array

--- 1.living/1.living.ino
+++ 1.living/1.living.ino
@@ -18,10 +18,12 @@ WiFiServer server(80);
 Button wallSwitch(wallSwitchPin);
 
 #include "src/Core/Devices/Light/Light.h"
-Light light1(out1, false); // main
-Light light2(out2, false); // dicro
-Light light3(out3, false); // bookshelf
-Light light4(out4, false); // corner
+Light light[] = {
+  Light(out1, false), // main
+  Light(out2, false), // dicro
+  Light(out3, false), // bookshelf
+  Light(out4, false) // corner
+};
 
 void setup() {
   pinMode(statusLedPin, OUTPUT);
@@ -30,11 +32,11 @@ void setup() {
   //core devices
   wallSwitch.setup();
   wallSwitch.swCallback=switchCallback;
-  light1.turnOn();
+  light[0].turnOn();
 
   //connect
-  wemosWiFi.connect("living");
-  light1.turnOff();
+  wemosWiFi.connect("dummy");
+  light[0].turnOff();
 
   // Start the server
   server.begin();
@@ -74,41 +76,24 @@ void handleConnection(){
   client.flush();
  
   // Match the request
-  if (request.indexOf("/status/1") != -1)  {
-    client.println("HTTP/1.1 200 OK");client.println("Content-Type: application/json");client.println(""); client.print("{\"statusPattern\": \""); client.print(light1.isOn?"true":"false"); client.println("\"}");
-    return;
-  }
+  if (request.indexOf("/status/") != -1)
+    return replyStatus(&client, &request);
 
-  if (request.indexOf("/status/2") != -1)  {
-    client.println("HTTP/1.1 200 OK");client.println("Content-Type: application/json");client.println(""); client.print("{\"statusPattern\": \""); client.print(light2.isOn?"true":"false"); client.println("\"}");
-    return;
-  }
-
-  if (request.indexOf("/status/3") != -1)  {
-    client.println("HTTP/1.1 200 OK");client.println("Content-Type: application/json");client.println(""); client.print("{\"statusPattern\": \""); client.print(light3.isOn?"true":"false"); client.println("\"}");
-    return;
-  }
-
-  if (request.indexOf("/status/4") != -1)  {
-    client.println("HTTP/1.1 200 OK");client.println("Content-Type: application/json");client.println(""); client.print("{\"statusPattern\": \""); client.print(light4.isOn?"true":"false"); client.println("\"}");
-    return;
-  }
-
-  if (request.indexOf("/set/1/on") != -1)     light1.turnOn();
-  if (request.indexOf("/set/1/off") != -1)    light1.turnOff();
-  if (request.indexOf("/set/1/toggle") != -1) light1.toggle();
+  if (request.indexOf("/set/1/on") != -1)     light[0].turnOn();
+  if (request.indexOf("/set/1/off") != -1)    light[0].turnOff();
+  if (request.indexOf("/set/1/toggle") != -1) light[0].toggle();
  
-  if (request.indexOf("/set/2/on") != -1)     light2.turnOn();
-  if (request.indexOf("/set/2/off") != -1)    light2.turnOff();
-  if (request.indexOf("/set/2/toggle") != -1) light2.toggle();
+  if (request.indexOf("/set/2/on") != -1)     light[1].turnOn();
+  if (request.indexOf("/set/2/off") != -1)    light[1].turnOff();
+  if (request.indexOf("/set/2/toggle") != -1) light[1].toggle();
  
-  if (request.indexOf("/set/3/on") != -1)     light3.turnOn();
-  if (request.indexOf("/set/3/off") != -1)    light3.turnOff();
-  if (request.indexOf("/set/3/toggle") != -1) light3.toggle();
+  if (request.indexOf("/set/3/on") != -1)     light[2].turnOn();
+  if (request.indexOf("/set/3/off") != -1)    light[2].turnOff();
+  if (request.indexOf("/set/3/toggle") != -1) light[2].toggle();
  
-  if (request.indexOf("/set/4/on") != -1)     light4.turnOn();
-  if (request.indexOf("/set/4/off") != -1)    light4.turnOff();
-  if (request.indexOf("/set/4/toggle") != -1) light4.toggle();
+  if (request.indexOf("/set/4/on") != -1)     light[3].turnOn();
+  if (request.indexOf("/set/4/off") != -1)    light[3].turnOff();
+  if (request.indexOf("/set/4/toggle") != -1) light[3].toggle();
  
  
   // Return the response
@@ -116,14 +101,10 @@ void handleConnection(){
   client.println("Content-Type: text/html");
   client.println(""); //  do not forget this one
   client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
- 
-  client.printf("io 1: %s <a href=\"/set/1/toggle\"\"><button>switch</button></a><br>", (light1.isOn?"On":"Off") );
-  client.printf("io 2: %s <a href=\"/set/2/toggle\"\"><button>switch</button></a><br>", (light2.isOn?"On":"Off") );
-  client.printf("io 3: %s <a href=\"/set/3/toggle\"\"><button>switch</button></a><br>", (light3.isOn?"On":"Off") );
-  client.printf("io 4: %s <a href=\"/set/4/toggle\"\"><button>switch</button></a><br>", (light4.isOn?"On":"Off") );
-
-  client.println("</html>");
+  client.println("<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head><body>");
+  for (size_t i = 0; i < 4; i++)
+    client.printf("io %i: %s <a href=\"/set/%i/toggle\"\"><button>switch</button></a><br>", i, (light[i].isOn?"On":"Off"), i );
+  client.println("</body></html>");
  
   delay(1);
   Serial.println("Client disonnected");
@@ -131,7 +112,18 @@ void handleConnection(){
 
 };
 
+void replyStatus(WiFiClient *cl, String *req){
+  
+  int st_from = req->indexOf("/status/") + 8;
+  int pos = req->substring(st_from, st_from+1).toInt()-1;
 
+  cl->println("HTTP/1.1 200 OK");
+  cl->println("Content-Type: application/json");
+  cl->println(""); 
+  cl->print("{\"statusPattern\": \""); 
+  cl->print(light[pos].isOn?"true":"false"); 
+  cl->println("\"}");
+}
 
 void switchCallback(int clicks) {
   Serial.print(clicks);
@@ -143,23 +135,23 @@ void switchCallback(int clicks) {
     turnAllOff();
     break;
   case 1:
-    if(light1.isOn){
+    if(light[0].isOn){
       turnAllOff();
     }else{
-      light1.turnOn();
+      light[0].turnOn();
     };
     break;
 
   case 2:
-    light2.toggle();
+    light[1].toggle();
     break;
 
   case 3:
-    light3.toggle();
+    light[2].toggle();
     break;
 
   case 4:
-    light4.toggle();
+    light[3].toggle();
     break;
 
   default:
@@ -168,8 +160,8 @@ void switchCallback(int clicks) {
 }
 
 void turnAllOff(){
-  light1.turnOff();
-  light2.turnOff();
-  light3.turnOff();
-  light4.turnOff();
+  light[0].turnOff();
+  light[1].turnOff();
+  light[2].turnOff();
+  light[3].turnOff();
 }
