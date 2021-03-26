dfe963bb9e60b100c3405ec0edd80da39da7fed3
DarkAngel <marcelo@darkblue.tv>
2021-03-26 10:22:41 -0300

    add refresh to controller ui

--- libs/Core/Web/WebServerHelper.h
+++ libs/Core/Web/WebServerHelper.h
@@ -29,7 +29,7 @@ public:
     void send_root(String html_devices, String node_name){
         String r_body;
         r_body += "<html>";
-        r_body += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><style>";
+        r_body += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><meta http-equiv=\"refresh\" content=\"60\"/><style>";
         r_body += "html,body{background-color:#000;color:#eee;font-family:sans-serif;margin:0;padding:0}";
         r_body += "div.button{width:8em;height:8em;display:inline-block;padding:1em;background-color:#222;margin:.25em;cursor:pointer}";
         r_body += "h1{background-color:#111;color:#ccc}";
