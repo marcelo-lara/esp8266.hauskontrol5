5e1f260b56e37e1d88fe2d5444940fdc9b2bb974
DarkAngel <marcelo@darkblue.tv>
2021-03-28 21:51:02 -0300

    webpanel | minimal list

--- 0.webpanel/index.js
+++ 0.webpanel/index.js
@@ -5,6 +5,7 @@ const haus = {
         {"name": "living", "uri": "192.168.1.122"},
         {"name": "suite", "uri": "192.168.1.126"},
         {"name": "office", "uri": "192.168.1.120"},
+        {"name": "officeac", "uri": "192.168.1.121"},
         {"name": "stage3", "uri": "192.168.1.61"}
     ]
 };
@@ -16,9 +17,6 @@ const haus = {
         console.log("::", controller.name, "["+ controller.uri+ "]");
         controller.obj = new Controller({name: controller.name, uri: controller.uri});
         controller.obj.discover();
-
     });
-
-
         
 })();
