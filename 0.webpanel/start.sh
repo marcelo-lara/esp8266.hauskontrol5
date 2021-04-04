8d5830ec6ecd4fa94a120b5cead2f00dbb572144
DarkAngel <marcelo@darkblue.tv>
2021-04-04 14:55:16 -0300

    webui | docker start script

--- 0.webpanel/start.sh
+++ 0.webpanel/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# stop/remove current container
+[ "$(docker ps -q -a -f name=hauskontrol-webpanel)" ] && docker rm -f hauskontrol-webpanel
+
+# build image
 docker build -t hauskontrol-webpanel .
 
-docker run -dit --name hauskontrol-webpanel -p 8080:80 hauskontrol-webpanel
+# run container
+docker run -dit --name hauskontrol-webpanel -p 80:80 hauskontrol-webpanel
