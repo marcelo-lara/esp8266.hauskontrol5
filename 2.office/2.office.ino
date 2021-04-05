288fc348975fb19e3e823a532f1eb62122693100
DarkAngel <marcelo@darkblue.tv>
2021-04-04 22:32:08 -0300

    WIP: office mqtt integration

--- 2.office/2.office.ino
+++ 2.office/2.office.ino
@@ -1,7 +1,6 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include "src/Core/Web/WebUi.h"
 #include "src/wemos.setup/wemos.setup.h"
+#include "src/Core/Web/WebUi.h"
 
 /////////////////////////////////////////
 // Hardware Setup
@@ -13,8 +12,13 @@
 #define bme280_sda      4 // D2 SDA (bme280)
 #define wifiLedPin      2 // D4 builtIn led
 
-#define NODE_NAME "office"
+#define NODE_NAME "dummyoffice"
 WebUi ui(Controller::Office, NODE_NAME);
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
 //// devices ////
 #include "src/Core/Devices/Button/Button.h"
@@ -26,6 +30,47 @@ Light light(relayOutPin, true);
 #include "src/Core/Devices/Environment/Environment.h"
 Environment environment;
 
+// MQTT: ID, server IP, port, username and password
+const PROGMEM char* MQTT_CLIENT_ID = "office_light1";
+const PROGMEM char* MQTT_SERVER_IP = "192.168.1.201";
+const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
+const PROGMEM char* MQTT_USER = "darkblue";
+const PROGMEM char* MQTT_PASSWORD = "mju76yhn";
+
+// MQTT: topics
+const char* MQTT_LIGHT_STATE_TOPIC = "office/light1/status";
+const char* MQTT_LIGHT_COMMAND_TOPIC = "office/light1/switch";
+// payloads by default (on/off)
+const char* LIGHT_ON = "ON";
+const char* LIGHT_OFF = "OFF";
+
+void mqtt_callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+  // concat the payload into a string
+  String payload;
+  for (uint8_t i = 0; i < p_length; i++) {
+    payload.concat((char)p_payload[i]);
+  }
+  
+  Serial.print("mqtt> ");
+  Serial.print(p_topic);
+  Serial.print(" | ");
+  Serial.println(payload);
+
+  if (String("homeassistant/light/office_light/state").equals(p_topic)) {
+      client.publish("homeassistant/light/office_light/state", "ON");
+  }
+
+  // handle message topic
+  if (String(MQTT_LIGHT_COMMAND_TOPIC).equals(p_topic)) {
+    // test if the payload is equal to "ON" or "OFF"
+    if (payload.equals(String(LIGHT_ON))) {
+        Serial.println("lightOn packet");
+    } else if (payload.equals(String(LIGHT_OFF))) {
+        Serial.println("lightOn packet");
+    }
+  }
+
+}
 
 void setup() {
   pinMode(statusLedPin, OUTPUT);
@@ -38,6 +83,10 @@ void setup() {
 
   //connect
   wemosWiFi.connect(NODE_NAME);
+
+  //mqtt connect
+  client.setServer("192.168.1.201", 1883);
+  client.setCallback(mqtt_callback);
 
   // Start the server
   ui.add_device(&light);
@@ -58,4 +107,33 @@ void loop() {
   wemosWiFi.update();
   ui.update();
 
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("dummyoffice", "darkblue", "mju76yhn")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("homeassistant/dummynode/light/state","ON");
+      
+      char* hass_config = "{\"~\": \"homeassistant/light/office\",  \"name\": \"Office Light\",  \"unique_id\": \"ctrl_office_light\",  \"cmd_t\": \"~/set\",  \"stat_t\": \"~/state\",  \"schema\": \"json\",  \"brightness\": true}";
+      client.publish("homeassistant/light/office/config", hass_config);
+      // ... and resubscribe
+      client.subscribe("homeassistant/light/office/state");
+      client.subscribe("homeassistant/light/office/set");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
