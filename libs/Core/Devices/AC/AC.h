b52db179fadeeb123fe257c6bb94cf24190f0960
Marcelo Lara <marcelo.lara@globant.com>
2021-04-20 21:56:50 -0300

    ac | Device implement

--- libs/Core/Devices/AC/AC.h
+++ libs/Core/Devices/AC/AC.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Arduino.h"
+#include "../Device.h"
 typedef void ACStatusChanged(bool isOn);
 
-class AC {
+class AC : public Device {
 public:
     AC(int _irOut, int defaultTemp);
     
