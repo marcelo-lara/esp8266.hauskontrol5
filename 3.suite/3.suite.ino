930d2650bbf6c7aa80a889b4298c1d4b43007f52
DarkAngel <marcelo@darkblue.tv>
2023-02-19 20:43:25 -0300

    suite | add heatingPanel

--- 3.suite/3.suite.ino
+++ 3.suite/3.suite.ino
@@ -5,11 +5,13 @@
 /////////////////////////////////////////
 // Hardware Setup
 #define wifiLedPin      2 // D4 builtIn led
-#define wallSwitchPin   5 // D1 Wall Switch
 #define statusLedPin    2 // D4 Wall StatusLed
 
 //light
 #define lightOut        4 // D2
+
+//heating panel
+#define heatingPanelOut    5 // D1 heating panel
 
 //fan
 #define fanOut1           13 // D7
@@ -22,25 +24,27 @@
 #define NODE_NAME "suite"
 WebApi api(WebApi::Controller::Suite);
 
-#include "src/Core/Devices/Button/Button.h"
-Button wallSwitch(wallSwitchPin);
-
 #include "src/Core/Devices/Light/Light.h"
-Light light(lightOut, true); // main
-void handleLight(String *args);
+Light light("main", lightOut, true); // main
+// void handleLight(String *args);
+
+// #include "src/Core/Devices/Button/Button.h"
+// Button wallSwitch(wallSwitchPin);
+Light heating("heating", heatingPanelOut, false); // main
 
 #include "src/Core/Devices/Fan/Fan.h"
 Fan fan(true, fanOut1, fanOut2, fanOut3, fanOut4, defSpeed); // suite fan
 
-Device* devices[] = {&light, &fan};
+Device* devices[] = {&light, &heating, &fan};
 
 
 void setup() {
 
   //core devices
-  wallSwitch.setup();
-  wallSwitch.swCallback=switchCallback;
+  // wallSwitch.setup();
+  // wallSwitch.swCallback=switchCallback;
   light.turnOn();
+  heating.turnOff();
 
   //connect
   wemosWiFi.connect(NODE_NAME);
@@ -50,6 +54,7 @@ void setup() {
   api.setup();
  
   //mqtt publish
+  heating.statusChanged=[](String topic, bool state){api.mqtt_publish(&heating);};
   light.statusChanged=[](String topic, bool state){api.mqtt_publish(&light);};
   fan.statusChanged=[](int outSpeed){api.mqtt_publish((Fan*)(&fan));};
 
@@ -62,32 +67,32 @@ void setup() {
 }
  
 void loop() {
-  wallSwitch.update();
+  // wallSwitch.update();
   wemosWiFi.update();
   api.update();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void switchCallback(int clicks) {
+// void switchCallback(int clicks) {
 
-  switch (clicks)
-  {
-  case BTN_CLICK:
-    light.toggle();
-    break;
+//   switch (clicks)
+//   {
+//   case BTN_CLICK:
+//     light.toggle();
+//     break;
   
-  case BTN_DBLCLICK:
-    fan.toggle();
-    break;
+//   case BTN_DBLCLICK:
+//     fan.toggle();
+//     break;
 
-  case BTN_LONGCLICK:
-    light.turnOff();
-    fan.turnOff();
-    break;
+//   case BTN_LONGCLICK:
+//     light.turnOff();
+//     fan.turnOff();
+//     break;
 
-  default:
-    break;
-  }
+//   default:
+//     break;
+//   }
     
-}
+// }
